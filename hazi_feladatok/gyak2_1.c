d292632af1d59d7c130c046c633f40400822361c
Goldiath <pircsi423@gmail.com>
2022-10-20 18:44:00 +0200

    sus

--- hazi_feladatok/gyak2_1.c
+++ hazi_feladatok/gyak2_1.c
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
     int a = 0, b = 0, c = 0;
 
     printf("Please enter the side lengths of a rectangle ('a,b,c'): ");
-    scanf("%d,%d,%d", &a, &b, &c);
+    scanf_s("%d,%d,%d", &a, &b, &c);
 
     int surface = 2 * (a * b + a * c + b * c);
     printf("Surface of rectangle: %d\n", surface);
---
+++ hazi_feladatok/gyak5_1.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(int argc, char const *argv[])
+{
+    int size = 10;
+    double array[size];
+
+    srand(time(0));
+
+    int upper = 320, lower = 270;
+    double range = upper - lower;
+    double div = RAND_MAX / range;
+    double value;
+
+    for (int i = 0; i < size; i++) {        
+        value = lower + (rand() / div);
+        array[i] = round(value * 100) / 100;
+    }
+
+    printf("Relative values after '%lf': ", array[0]);
+
+    for (int i = 1; i < size; i++) {
+        printf("%.1lf ", array[i] - array[0]);
+    }
+    
+    printf("\n");
+
+    if (array[1] < 0) {        
+        printf("Values aren't monotone.");
+        return 0;
+    } else {
+        for (int i = 2; i < size; i++)
+        {
+            if (array[i] < array[i - 1]){
+                printf("Values aren't monotone.");
+                return 0;  
+            }
+        }
+        printf("Values are monotone.");
+    }
+
+    return 0;
+}
