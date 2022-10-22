50a619ed0d7e652cf8c31dc46f6e8c84facc0cee
Goldiath <pircsi423@gmail.com>
2022-10-22 17:25:03 +0200

    oop

--- hazi_feladatok/gyak5_2.c
+++ hazi_feladatok/gyak5_2.c
@@ -3,9 +3,25 @@
 
 int main(int argc, char const *argv[])
 {
-    float grades[6];
+    float gs[6];
+    
+    int ok;
+    do {
+        ok = 1;
+        printf("Enter six grades ('x,y,z...'): ");
+        if (scanf("%f,%f,%f,%f,%f,%f", &gs[0], &gs[1], &gs[2], &gs[3], &gs[4], &gs[5]) != 6) {
+            printf("Invalid input.\n");
+            for (int i = 0; i < 6; i++) {
+                while ((gs[i] = getchar()) != '\n');
+            }
+            ok = 0;
+        }
+    } while (!ok);
 
-    printf("Enter six grades ('x,y,z...'): ");
+    printf("Inputted grades:");
+    for (int i = 0; i < 6; i++) {
+        printf(" %f", gs[0]);
+    }
     
     
 
