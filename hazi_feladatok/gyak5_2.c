e06deb736852ae0ff26e04c8923f88189913c950
Goldiath <pircsi423@gmail.com>
2022-10-22 10:54:33 +0200

    new

---
+++ hazi_feladatok/gyak5_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    float grades[6];
+
+    printf("Enter six grades ('x,y,z...'): ");
+    
+    
+
+
+    return 0;
+}
